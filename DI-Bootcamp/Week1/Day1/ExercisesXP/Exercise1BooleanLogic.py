bdaa0add37e368b04ef0d94099b62d7941e83902
yaouate <25363285+yaouate@users.noreply.github.com>
2026-02-11 01:31:10 +0200

    exo1

---
+++ DI-Bootcamp/Week1/Day1/ExercisesXP/Exercise1BooleanLogic.py
@@ -0,0 +1,26 @@
+first = "Hello World"
+# This is a comment.
+print("I AM A COMPUTER!")
+if 1<2 and 4>2:
+    print("Math is fun.")
+nope = None
+if 2>3 and 2<3:
+     print("Math is not fun.")
+else: print("Math is realy not fun, This calculation is False")
+print (len ("What's my length?"))
+test= int("1000")
+print(type(test))
+print (str(4)+"real")
+print (3 * "cool")
+print("ZeroDivisionError: division by zero")
+print (type([]))
+name=input("write your name please.")
+print (name)
+user_input=input("write a positive number.")
+number = float(user_input)
+if number > 0:
+    print ("That number is greater than 0!")
+if number < 0:
+    print ("That number is less than 0!")
+if number == 0:
+    print ("You picked 0!")
