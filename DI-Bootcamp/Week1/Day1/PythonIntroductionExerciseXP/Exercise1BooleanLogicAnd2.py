72587673ec1c7bf180fd961af68b9fa0e99e12e3
yaouate <25363285+yaouate@users.noreply.github.com>
2026-02-11 11:57:00 +0200

    rename2

--- DI-Bootcamp/Week1/Day1/PythonIntroductionExerciseXP/Exercise1BooleanLogicAnd2.py
+++ DI-Bootcamp/Week1/Day1/PythonIntroductionExerciseXP/Exercise1BooleanLogicAnd2.py
@@ -1,3 +1,4 @@
+#exercise 1
 first = "Hello World"
 # This is a comment.
 print("I AM A COMPUTER!")
@@ -29,3 +30,18 @@ print (index.find('l'))
 print ("y" in "xylophone")
 string = "my_string"
 print (string.islower())
+
+#Exercise 2
+humanYears=10
+catYears1=15
+catYears2=15+9
+catYears3andMore=15+9+(4*(humanYears-2))
+dogYears1=15
+dogYears2=15+9
+dogYears3andMore=15+9+(5*(humanYears-2))
+if humanYears==1:
+    print (humanYears,catYears1,dogYears1)
+if humanYears==2:
+    print (humanYears,catYears2,dogYears2)
+if humanYears>2:
+    print (humanYears,catYears3andMore,dogYears3andMore)
